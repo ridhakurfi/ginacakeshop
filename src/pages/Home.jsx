40da67c90ab2d34410ad47f191dfc1d9149d75c0
ridhakurfi <ridhakurfi@gmail.com>
2026-06-25 07:29:52 +0700

    added carousel hard coded array +2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,53 +3,7 @@ import "../styles/home.css";
 
 function Home() {
     const cakes = [
-        {
-            name: "Strawberry Cheesecake",
-            price: "Rp 200,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345296/Kue%20Gina/Strawberry_Cheesecake_xtdswh.jpg",
-        },
-        {
-            name: "Blueberry Cheesecake",
-            price: "Rp 180,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345294/Kue%20Gina/Blueberry_Cheesecake_bitzjw.jpg",
-        },
-        {
-            name: "Triple Chocolate Cake",
-            price:
-                "Rp 225,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345297/Kue%20Gina/Triple_Chocolate_Cake_xwtwyd.jpg",
-        },
-        {
-            name: "Mix Fruit Cake",
-            price:
-                "Rp 325,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345294/Kue%20Gina/Mix_Fruit_Cake_k3faiq.jpg",
-        },
-        {
-            name: "Mango Cake",
-            price:
-                "Rp 280,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345294/Kue%20Gina/Mango_Cake_f4oo3f.jpg",
-        },
-        {
-            name: "Mocca Nougat Cake",
-            price:
-                "Rp 275,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345295/Kue%20Gina/Mocca_Nougat_Cake_u5cbg6.jpg",
-        },
-        {
-            name: "Oreo Strawberry Cheesecake",
-            price:
-                "Rp 255,000",
-            image:
-                "https://res.cloudinary.com/ducjx48tv/image/upload/v1782345296/Kue%20Gina/Oreo_Strawberry_Cheesecake_it5pwx.jpg",
-        },
+        
     ];
 
     const [current, setCurrent] = useState(0);
